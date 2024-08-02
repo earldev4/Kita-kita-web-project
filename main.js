1785a240b79a9571df0e609f6d1f5149838621e2
Deva Ahmad <devaahmad43@gmail.com>
2024-08-02 21:37:12 +0700

    membuat animasi chat baloon

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let pict1 = document.getElementById('h-pict1')
 let pict2 = document.getElementById('h-pict2')
 let pict3 = document.getElementById('h-pict3')
 let pict4 = document.getElementById('h-pict4')
-let phone = document.getElementById('phone')
+const baloon = document.querySelectorAll('.chat-baloon')
+const right_baloon = document.getElementsByClassName('right-chat')
+
 
 window.addEventListener('scroll', () =>{
     let value = window.scrollY;
@@ -22,7 +24,31 @@ window.addEventListener('scroll', () =>{
     pict4.style.top = value * -0.5 + 'px';
     pict4.style.left = value * -0.5 + 'px';
     pict4.style.opacity = opacity; 
+   
     
-    
-    console.log(value)
 });
+
+const observer = new IntersectionObserver(entries =>{
+    console.log(entries)
+    entries.forEach(entry =>{
+        entry.target.classList.toggle("swipe-right", entry.isIntersecting)
+    })
+}, {
+    // threshold: 1,
+    
+} )
+
+baloon.forEach(each=>{
+    observer.observe(each)
+})
+
+const rightobserver = new IntersectionObserver(entries =>{
+    entries.forEach(entry=>{
+        entry.target.classList.toggle("swipe-left", entry.isIntersecting)
+    })
+})
+
+Array.from(right_baloon).forEach(each =>{
+    rightobserver.observe(each)
+})
+
